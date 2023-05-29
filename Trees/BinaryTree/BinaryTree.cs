222226d56ae10546d3ad97562db484430c07b66f
Sadikcan Tuluk <sadikcantuluk@gmail.com>
2023-05-29 22:40:19 +0300

    LevelOrder(),FindMin(),FindMax(),Find(),Remove(),MaxDepth(),DeepestNode(),NumberOfLeafs() added.

--- Trees/BinaryTree/BinaryTree.cs
+++ Trees/BinaryTree/BinaryTree.cs
@@ -13,40 +13,247 @@ namespace Trees.BinaryTree
         {
             nodeList = new List<Node<T>>();
         }
-        public List<Node<T>> InOrder(Node<T> root)
+        public List<Node<T>> InOrderRecursion(Node<T> root)
         {
             if (!(root == null))
             {
-                InOrder(root.Left);
+                InOrderRecursion(root.Left);
                 //Console.WriteLine(root);
                 nodeList.Add(root);
-                InOrder(root.Right);
+                InOrderRecursion(root.Right);
             }
             return nodeList;
         }
 
-        public List<Node<T>> PreOrder(Node<T> root)
+        public List<Node<T>> InOrderIteratif(Node<T> root)
         {
-            if (!(root == null))
+            var stack = new Stack<Node<T>>();
+            bool done = false;
+            Node<T> currentNode = root;
+
+            while (!done)
             {
-                //Console.WriteLine(root);
-                nodeList.Add(root);
-                PreOrder(root.Left);
-                PreOrder(root.Right);
+                if (currentNode != null)
+                {
+                    stack.Push(currentNode);
+                    currentNode = currentNode.Left;
+                }
+                else
+                {
+                    if (stack.Count == 0)
+                    {
+                        done = true;
+                    }
+                    else
+                    {
+                        currentNode = stack.Pop();
+                        nodeList.Add(currentNode);
+                        currentNode = currentNode.Right;
+                    }
+                }
             }
             return nodeList;
         }
 
-        public List<Node<T>> PostOrder(Node<T> root)
+        public List<Node<T>> PreOrderRecursion(Node<T> root)
         {
             if (!(root == null))
             {
-                PostOrder(root.Left);
-                PostOrder(root.Right);
+                //Console.WriteLine(root);
+                nodeList.Add(root);
+                PreOrderRecursion(root.Left);
+                PreOrderRecursion(root.Right);
+            }
+            return nodeList;
+        }
+
+        public List<Node<T>> PreOrderIteratif(Node<T> root)
+        {
+            var stack = new Stack<Node<T>>();
+
+            if (root == null)
+                throw new Exception("The tree is empty.");
+
+            stack.Push(root);
+
+            while (!(stack.Count == 0))
+            {
+                var temp = stack.Pop();
+                nodeList.Add(temp);
+                if (temp.Right != null)
+                {
+                    stack.Push(temp.Right);
+                }
+                if (temp.Left != null)
+                {
+                    stack.Push(temp.Left);
+                }
+            }
+            return nodeList;
+        }
+
+        public List<Node<T>> PostOrderRecursion(Node<T> root)
+        {
+            if (!(root == null))
+            {
+                PostOrderRecursion(root.Left);
+                PostOrderRecursion(root.Right);
                 //Console.WriteLine(root);
                 nodeList.Add(root);
             }
             return nodeList;
+        }
+
+        public List<Node<T>> PostOrderIteratif(Node<T> root)
+        {
+            var stack = new Stack<Node<T>>();
+            bool done = false;
+            Node<T> currentNode = root;
+
+            while (!done)
+            {
+                if (currentNode != null)
+                {
+                    stack.Push(currentNode);
+                    currentNode = currentNode.Left;
+                }
+                else
+                {
+                    if (stack.Count == 0)
+                    {
+                        done = true;
+                    }
+                    else
+                    {
+                        currentNode = stack.Pop();
+                        nodeList.Add(currentNode);
+                        currentNode = currentNode.Right;
+                    }
+                }
+            }
+            return nodeList;
+        }
+
+        //public List<Node<T>> LevelOrderTraversal(Node<T> root)
+        //{
+        //    var queue = new Queue<Node<T>>();
+        //    Node<T> leftNode = root.Left;
+        //    Node<T> rightNode = root.Right;
+
+        //    queue.Enqueue(root);
+
+        //    while (queue.Count > 0)
+        //    {
+        //        var temp = queue.Dequeue();
+        //        nodeList.Add(temp);
+
+        //        if (leftNode != null)
+        //        {
+        //            queue.Enqueue(leftNode);
+        //            if (leftNode.Left != null)
+        //            {
+        //                leftNode = leftNode.Left;
+        //            }
+        //        }
+
+        //        if (rightNode != null)
+        //        {
+        //            queue.Enqueue(rightNode);
+        //            if (rightNode.Right != null)
+        //            {
+        //                rightNode = rightNode.Right;
+        //            }
+        //        }
+        //    }
+
+        //    return nodeList;
+        //}
+
+        public List<Node<T>> LevelOrderTraversal(Node<T> root)
+        {
+            var queue = new Queue<Node<T>>();
+            queue.Enqueue(root);
+            while (queue.Count > 0)
+            {
+                var temp = queue.Dequeue();
+                nodeList.Add(temp);
+                if (temp.Left != null)
+                    queue.Enqueue(temp.Left);
+                if (temp.Right != null)
+                    queue.Enqueue(temp.Right);
+            }
+            return nodeList;
+        }
+
+        public static int MaxDepth(Node<T> root)
+        {
+            if (root == null)
+                return 0;
+
+            int leftDepth = MaxDepth(root.Left);
+            int rightDepth = MaxDepth(root.Right);
+
+            return leftDepth > rightDepth ? leftDepth + 1 : rightDepth + 1;
+        }
+
+        public Node<T> DeepestNode(Node<T> root)
+        {
+            if (root == null)
+                throw new Exception();
+
+            Node<T> temp = null;
+            var q = new Queue<Node<T>>();
+
+            q.Enqueue(root);
+
+            while (q.Count > 0)
+            {
+                temp = q.Dequeue();
+                if (temp.Left != null)
+                {
+                    q.Enqueue(temp.Left);
+                }
+                if (temp.Right != null)
+                {
+                    q.Enqueue(temp.Right);
+                }
+            }
+            return temp;
+        }
+
+        public static int NumberOfLeafs(Node<T> root)
+        {
+            //var count = 0;
+
+            //if (root == null)
+            //    throw new Exception();
+
+            //var q = new Queue<Node<T>>();
+
+            //q.Enqueue(root);
+
+            //while (q.Count > 0)
+            //{
+            //    var temp = q.Dequeue();
+
+            //    if (temp.Left == null && temp.Right == null)
+            //    {
+            //        count++;
+            //    }
+
+            //    if (temp.Left != null)
+            //    {
+            //        q.Enqueue(temp.Left);
+            //    }
+            //    if (temp.Right != null)
+            //    {
+            //        q.Enqueue(temp.Right);
+            //    }
+            //}
+            //return count;
+
+            //ALTERNATİF
+            return new BinaryTree<T>().LevelOrderTraversal(root).Where(x => x.Left == null && x.Right == null).ToList().Count;
         }
 
 
--- Trees/Program.cs
+++ Trees/Program.cs
@@ -13,20 +13,56 @@ namespace Trees
     {
         static void Main(string[] args)
         {
-            var BST = new BinarySearchTree<int>(new int[] { 23, 16, 45, 3, 22, 37, 99 });
+            var BST = new BinarySearchTree<int>(new int[] { 40, 45, 20, 70, 85, 65, 60 });
 
-            //InOrder
-            new BinaryTree<int>().InOrder(BST.Root).ForEach(x => Console.Write(x + " "));
+            ////InOrderRecursive
+            //new BinaryTree<int>().InOrderRecursion(BST.Root).ForEach(x => Console.Write(x + " "));
 
-            Console.WriteLine();
+            //Console.WriteLine();
 
-            //PerOrder
-            new BinaryTree<int>().PreOrder(BST.Root).ForEach(x => Console.Write(x + " "));
+            ////InOrderİteratif
+            //new BinaryTree<int>().InOrderIteratif(BST.Root).ForEach(x => Console.Write(x + " "));
 
-            Console.WriteLine();
+            ////PerOrderRecursive
+            //new BinaryTree<int>().PreOrderRecursion(BST.Root).ForEach(x => Console.Write(x + " "));
 
-            //PostOrder
-            new BinaryTree<int>().PostOrder(BST.Root).ForEach(x => Console.Write(x + " "));
+            //Console.WriteLine();
+
+            ////PerOrderIteratif
+            //new BinaryTree<int>().PreOrderIteratif(BST.Root).ForEach(x => Console.Write(x + " "));
+
+            //Console.WriteLine();
+
+            ////PostOrderRecursive
+            //new BinaryTree<int>().PostOrderRecursion(BST.Root).ForEach(x => Console.Write(x + " "));
+
+            //Console.WriteLine();
+
+            ////PostOrderIteratif
+            //new BinaryTree<int>().PostOrderIteratif(BST.Root).ForEach(x => Console.Write(x + " "));
+
+            ////Level Order
+            //new BinaryTree<int>().LevelOrderTraversal(BST.Root).ForEach(x => Console.Write(x + " "));
+
+            //Console.WriteLine($"Max Value: {BST.FindMax(BST.Root)}"); 
+            //Console.WriteLine($"Min Value: {BST.FindMin(BST.Root)}"); 
+
+            //var key = BST.Find(BST.Root,55);
+            //Console.WriteLine($"Value:{key.Value} Left:{key.Left.Value} Right:{key.Right.Value}");
+
+            ////REMOVE
+            //BST.Remove(BST.Root,20);
+            //new BinaryTree<int>().InOrderRecursion(BST.Root).ForEach(x => Console.Write(x + " "));
+
+            //var depth = BinaryTree<int>.MaxDepth(BST.Root);
+            //Console.WriteLine(depth);
+
+            //var deepestValue = new BinaryTree<int>().DeepestNode(BST.Root);
+            //Console.WriteLine(deepestValue);
+
+            //NUMBER OF LEAFS
+            Console.WriteLine(BinaryTree<int>.NumberOfLeafs(BST.Root));
+
 
 
             Console.ReadKey();
