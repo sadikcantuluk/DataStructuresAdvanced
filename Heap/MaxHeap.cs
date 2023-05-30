30b6c9a176064f8433587ae4cc68f8d4c6a47337
Sadikcan Tuluk <sadikcantuluk@gmail.com>
2023-05-31 00:12:10 +0300

    Heap project added.

---
+++ Heap/MaxHeap.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heap
+{
+    public class MaxHeap<T> : BHeap<T>, IEnumerable<T> where T : IComparable
+    {
+        public MaxHeap() : base()
+        {
+
+        }
+        public MaxHeap(int size) : base(size)
+        {
+
+        }
+        public MaxHeap(IEnumerable<T> collection) : base(collection)
+        {
+
+        }
+        protected override void HeapfyDown()
+        {
+            int index = 0;
+            while (HasLeftChild(index))
+            {
+                var smallerindex = GetLeftChildIndex(index);
+                if (HasRightChild(index) && GetRightChild(index).CompareTo(GetLeftChild(index)) > 0)
+                {
+                    smallerindex = GetLeftChildIndex(index);
+                }
+                if (Array[smallerindex].CompareTo(Array[index]) < 0)
+                {
+                    break;
+                }
+
+                Swap(smallerindex, index);
+                index = smallerindex;
+            }
+        }
+
+        protected override void HeapfyUp()
+        {
+            int index = position - 1;
+            while (!IsRoot(index) && Array[index].CompareTo(GetParent(index)) > 0)
+            {
+                var parentIndex = GetParentIndex(index);
+                Swap(parentIndex, index);
+                index = parentIndex;
+            }
+        }
+    }
+}
---
+++ Heap/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var heap = new MinHeap<int>(new int[] { 4, 1, 10, 8, 7, 5, 9, 3, 2 });
+
+            //Console.WriteLine($"{heap.DeleteMinMax()} silindi.");
+
+            //foreach (var item in heap)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            var heap = new MaxHeap<int>() { 54, 45, 36, 27, 29, 18, 21, 99 };
+
+            Console.WriteLine($"{heap.DeleteMinMax()} silindi.");
+
+            foreach (var item in heap)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
